371174f5756772a961dbd2ef85d71a4b936e3a8b
Mathias <mfred@tuta.com>
2026-04-21 20:26:52 +0200

    added ether and ip header parsing

--- src/main.c
+++ src/main.c
@@ -13,13 +13,82 @@
 #include <stdatomic.h>
 
 #define SNAP_LEN 1518
+#define ETHER_LEN 14
+#define ETHER_ADDR_LEN 6
 
 static atomic_int count = 0;
+
+struct ethernet_hdr {
+	u_char ether_dhost[ETHER_ADDR_LEN];
+	u_char ether_shost[ETHER_ADDR_LEN];
+	u_short ether_type;
+} __attribute__((packed));
+
+struct ip_hdr {
+	u_char ip_vhl;
+	u_char ip_tos;
+	u_short ip_len;
+	u_short ip_id;
+	u_short ip_off;
+	u_char ip_ttl;
+	u_char ip_p;
+	u_short ip_sum;
+	struct in_addr ip_src, ip_dst;
+} __attribute__((packed));
+
+#define IP_V(ip) (((ip)->ip_vhl) >> 4)
+
+static int parse_ethernet_hdr(const u_char* packet, bpf_u_int32 caplen, struct ethernet_hdr* hdr)
+{
+	if (caplen < ETHER_LEN)
+		return 1;
+
+	memcpy(hdr, packet, sizeof(struct ethernet_hdr));
+	if (hdr == NULL)
+		return 1;
+
+	hdr->ether_type = ntohs(hdr->ether_type);
+
+	if (hdr->ether_type != 0x800)
+		return 1;
+
+	return 0;
+}
+
+static int parse_ip_hdr(const u_char* packet, bpf_u_int32 caplen, struct ip_hdr* hdr)
+{
+	if (caplen < ETHER_LEN + sizeof(struct ip_hdr))
+		return 1;
+
+	memcpy(hdr, packet + ETHER_LEN, sizeof(struct ip_hdr));
+	if (hdr == NULL)
+		return 1;
+
+	if (IP_V(hdr) != 4) {
+		printf("not ipv4: \n", IP_V(hdr));
+		return 1;
+	}
+
+	hdr->ip_len = ntohs(hdr->ip_len);
+	hdr->ip_id = ntohs(hdr->ip_id);
+	hdr->ip_off = ntohs(hdr->ip_off);
+	hdr->ip_sum = ntohs(hdr->ip_sum);
+
+	return 0;
+}
 
 static void got_packet(u_char* args, const struct pcap_pkthdr* header, const u_char* packet)
 {
 	int curr_count = atomic_fetch_add_explicit(&count, 1, memory_order_relaxed);
 	printf("got packet: %d\n", curr_count);
+
+	struct ethernet_hdr eth_hdr;
+	if (parse_ethernet_hdr(packet, header->caplen, &eth_hdr) == 0)
+		printf("parsed eth hdr\n");
+
+	struct ip_hdr ip_hdr;
+	if (parse_ip_hdr(packet, header->caplen, &ip_hdr) == 0)
+		printf("parsed ip hdr\n");
 }
 
 int main(int argc, char* argv[])
