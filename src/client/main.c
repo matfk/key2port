39526b47819a03f317a1a657e8c4a7beb97c5c6f
Mathias <mfred@tuta.com>
2026-05-04 17:15:38 +0200

    added sending functionality from client to server

--- src/client/main.c
+++ src/client/main.c
@@ -3,14 +3,27 @@
 #include <stdlib.h>
 #include <time.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netinet/in.h>
 #include <core/string.h>
 #include <libspa/key.h>
 #include <libspa/spa.h>
 #include <sodium.h>
 
+#define MIN_PORT 1025
+#define MAX_PORT 65535
+
 void print_usage()
 {
 	printf("usage: <payload>\n");
+}
+
+u16 random_port()
+{
+	u16 port = rand() % (MAX_PORT - MIN_PORT + 1) + MIN_PORT;
+	return port;
 }
 
 int main(int argc, char* argv[])
@@ -32,16 +45,14 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	printf("pem: %s\n", pem);
-
-	uint8_t seed[crypto_sign_SEEDBYTES];
+	u8 seed[crypto_sign_SEEDBYTES];
 	if (parse_openssh_priv_pem(pem, seed) != 0) {
 		printf("Could not parse private key pem\n");
 		return 1;
 	}
 
-	uint8_t pk[crypto_sign_PUBLICKEYBYTES];
-	uint8_t sk[crypto_sign_SECRETKEYBYTES];
+	u8 pk[crypto_sign_PUBLICKEYBYTES];
+	u8 sk[crypto_sign_SECRETKEYBYTES];
 	if (crypto_sign_seed_keypair(pk, sk, seed) != 0) {
 		printf("Could not sign seed bytes\n");
 		return 1;
@@ -57,7 +68,7 @@ int main(int argc, char* argv[])
 	char* msg = argv[1];
 	spa_hdr.payload_len = strlen(msg);
 
-	uint8_t* packet = NULL;
+	u8* packet = NULL;
 	size_t packet_len = 0;
 	if (spa_build_packet(&spa_hdr, msg, sk, &packet, &packet_len) != 0) {
 		fprintf(stderr, "spa build failed\n");
@@ -66,6 +77,30 @@ int main(int argc, char* argv[])
 
 	printf("packet len: %d\n", packet_len);
 
+	// Sending packet
+	struct sockaddr_in servaddr;
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	servaddr.sin_port = htons(random_port());
+	servaddr.sin_family = AF_INET;
+
+	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd == -1) {
+		perror("sockfd");
+		free(packet);
+		return 1;
+	}
+
+	if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+		perror("connect");
+		free(packet);
+		return 1;
+	}
+
+	sendto(sockfd, packet, packet_len, 0, (struct sockaddr*)NULL, sizeof(servaddr));
+
 	free(packet);
+	close(sockfd);
 	return 0;
 }
