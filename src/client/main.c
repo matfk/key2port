32b4219862e79496c321be1fe6d41a93ecd22b44
Mathias <mfred@tuta.com>
2026-05-06 22:51:16 +0200

    cleaned up client

--- src/client/main.c
+++ src/client/main.c
@@ -46,79 +46,87 @@ u16 random_port()
 	return (u16)(rand() % (MAX_PORT - MIN_PORT + 1) + MIN_PORT);
 }
 
-int send_packet()
+int parse_pem_sk(const char* path, u8 sk[crypto_sign_SECRETKEYBYTES])
 {
-	FILE* sk_file = fopen("key", "rb");
+	FILE* sk_file = fopen(path, "rb");
 	if (sk_file == NULL) {
 		perror("fopen");
-		return 1;
+		return -1;
 	}
 
 	char pem[4096];
 	if (read_to_string(pem, sizeof(pem), sk_file) != 0) {
 		perror("read_to_string");
-		return 1;
+		return -1;
 	}
+
+	fclose(sk_file);
 
 	u8 seed[crypto_sign_SEEDBYTES];
 	if (parse_openssh_priv_pem(pem, seed) != 0) {
-		printf("Could not parse private key pem\n");
-		return 1;
+		fprintf(stderr, "parse_pem_sk: failed to parse pem\n");
+		return -1;
 	}
 
 	u8 pk[crypto_sign_PUBLICKEYBYTES];
-	u8 sk[crypto_sign_SECRETKEYBYTES];
 	if (crypto_sign_seed_keypair(pk, sk, seed) != 0) {
-		printf("Could not sign seed bytes\n");
-		return 1;
+		fprintf(stderr, "parse_pem_sk: failed to sign seed keypair\n");
+		return -1;
 	}
 
+	return 0;
+}
+
+struct spa_hdr create_packet_hdr(u32 client_id, u16 flags)
+{
 	struct spa_hdr spa_hdr;
 	spa_hdr.magic = SPA_MAGIC;
 	spa_hdr.version = SPA_VERSION;
-	spa_hdr.flags = 0;
-	spa_hdr.client_id = 2026;
+	spa_hdr.flags = flags;
+	spa_hdr.client_id = client_id;
 	spa_hdr.timestamp = (u32)time(NULL);
 	randombytes_buf(spa_hdr.nonce, SPA_NONCE_LEN);
+	return spa_hdr;
+}
 
-	u16 port = 4444;
-	struct spa_payload payload = { 120, port };
-	spa_hdr.payload_len = sizeof(payload);
+int create_packet(u16 port, u32 ttl, u32 client_id, u16 flags, u8 sk[crypto_sign_SECRETKEYBYTES], u8** packet, size_t* packet_len)
+{
+	struct spa_hdr hdr = create_packet_hdr(client_id, flags);
+	struct spa_payload payload = { ttl, port };
+	hdr.payload_len = sizeof(payload);
 
-	u8* packet = NULL;
-	size_t packet_len = 0;
-	if (spa_build_packet(&spa_hdr, (const u8*)&payload, sk, &packet, &packet_len) != 0) {
+	if (spa_build_packet(&hdr, (const u8*)&payload, sk, packet, packet_len) != 0) {
 		fprintf(stderr, "spa build failed\n");
-		return 1;
+		return -1;
 	}
 
-	printf("packet len: %d\n", packet_len);
+	return 0;
+}
 
-	// Sending packet
+int send_udp_packet(const char* target, const u8* packet, size_t packet_len)
+{
 	struct sockaddr_in servaddr;
-
 	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	servaddr.sin_addr.s_addr = inet_addr(target);
 	servaddr.sin_port = htons(random_port());
 	servaddr.sin_family = AF_INET;
 
 	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sockfd == -1) {
 		perror("sockfd");
-		free(packet);
-		return 1;
+		return -1;
 	}
 
 	if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
 		perror("connect");
-		free(packet);
-		return 1;
+		return -1;
 	}
 
 	sendto(sockfd, packet, packet_len, 0, (struct sockaddr*)NULL, sizeof(servaddr));
-
-	free(packet);
 	close(sockfd);
+
+	return 0;
 }
 
 int main(int argc, char* argv[])
@@ -128,6 +136,7 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 
+	u32 client_id = 2000;
 	u16 port = 4444;
 	u32 ttl = 120;
 	int opt;
@@ -155,5 +164,19 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
+	u8 sk[crypto_sign_SECRETKEYBYTES];
+	if (parse_pem_sk("key", sk) != 0)
+		return 1;
+
+	u8* packet = NULL;
+	size_t packet_len = 0;
+
+	if (create_packet(port, ttl, client_id, 0, sk, &packet, &packet_len) != 0)
+		return 1;
+
+	if (send_udp_packet(target, packet, packet_len) != 0)
+		return 1;
+
+	free(packet);
 	return 0;
 }
